637613d61768c17af87cd1aecd9b84002ca73c72
samurai <mathewsloban@yandex.com>
2021-09-01 16:55:45 +0300

    I create directory [model] and realization classes data. Create directory [data] and realization class [TestData]

---
+++ first-angular-project/src/app/data/TestData.ts
@@ -0,0 +1,17 @@
+import {Category} from "../model/Category";
+
+export class TestData{
+
+  static categories: Category[] = [
+    {id:1,title:'Работа'},
+    {id:2,title:'Семья'},
+    {id:3,title:'Учёба'},
+    {id:4,title:'Отдых'},
+    {id:5,title:'Спорт'},
+    {id:6,title:'Еда'},
+    {id:7,title:'Финансы'},
+    {id:8,title:'Гаджеты'},
+    {id:9,title:'Здоровье'},
+    {id:10,title:'Автомобиль'},
+  ];
+}
