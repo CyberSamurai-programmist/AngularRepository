477a0ee03613b06cad147ff772dd2cc46d226206
samurai <mathewsloban@yandex.com>
2021-09-02 09:36:43 +0300

    Create new Component [NavigatePanel] and new Service [DataHandler].Decomposition HTML file [app.component.html]

--- first-angular-project/src/app/app.module.ts
+++ first-angular-project/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigatePanelComponent } from './views/navigate-panel/navigate-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigatePanelComponent,
   ],
   imports: [
     BrowserModule
---
+++ first-angular-project/src/app/views/navigate-panel/navigate-panel.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavigatePanelComponent } from './navigate-panel.component';
+
+describe('NavigatePanelComponent', () => {
+  let component: NavigatePanelComponent;
+  let fixture: ComponentFixture<NavigatePanelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavigatePanelComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavigatePanelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
