35db65980b2cdcd29428912a5745ea7febc25943
samurai <mathewsloban@yandex.com>
2021-09-02 16:52:22 +0300

    Edit Component and Service

--- first-angular-project/src/app/views/navigate-panel/navigate-panel.component.ts
+++ first-angular-project/src/app/views/navigate-panel/navigate-panel.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Category} from "../../model/Category";
 
 @Component({
   selector: 'app-navigate-panel',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigatePanelComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
 
+  constructor(private dataHandler: DataHandlerService) {
+  }
+
+  // метод вызывается автоматически после инициализации компонента
   ngOnInit(): void {
+    this.categories = this.dataHandler.getCategories();
+    console.log(this.categories);
   }
 
 }
